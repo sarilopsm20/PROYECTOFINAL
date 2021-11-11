0bb03aca7a96f7a3b45953e74194c517527ed27c
sarilopsm20 <44045585+sarilopsm20@users.noreply.github.com>
2021-11-10 23:40:01 -0500

    Ammm

---
+++ Fecha.js
@@ -0,0 +1,10 @@
+var hoy = new Date();
+var dia=new Array(7);
+    dia[0]="Domingo";
+    dia[1]="Lunes";
+    dia[2]="Martes";
+    dia[3]="Miércoles";
+    dia[4]="Jueves";
+    dia[5]="Viernes";
+    dia[6]="Sabado";
+var diaS = document.write("Hoy es: ", dia[hoy.getDay()]+ " " + hoy.getDate());
